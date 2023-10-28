65829d27749c3126acbdc24368d7357bec57b187
Sachendra79 <sachendragangwar120@gmail.com>
2023-10-28 17:50:02 +0530

    emailvalidation in loginform

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -6,21 +6,34 @@ import side from '../Image/side.png'
 import  { useState } from 'react';
 
 function Login({children}) {
- const[email,setEmail]=useState('');
-const [isValidEmail,setisvalidEmail]=useState(true);
-const [btndisable,setbtnDisable]=useState(true);
-const [notice,setNotice]=useState("");
-const [password,setPassword]=useState("");
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({ email: '', password: '' });
+
+
+  function validateInput (name, value)  {
+    const errorsCopy = { ...errors };
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const passwordRegex = /^2.{7,}$/; 
+
+   if(name=== 'email')
+   {
+    errorsCopy.email = emailRegex.test(value) ? '': 'Invalid email format';
+   }
+    if(name=== 'password')
+    {
+      errorsCopy.password = passwordRegex.test(value) ? '': 'Wrong Password';
+    }
+  
+    setErrors(errorsCopy);
+  };
+
 
 function changeHandler(e)
 {
-    const inputemail=e.target.value;
-    setEmail(inputemail);
+  const { name, value } = e.target;
+  setFormData({ ...formData, [name]: value });
+  validateInput(name, value);
 };
-function passwordchangeHandler(e)
-{
-
-}
 
   return (
 
@@ -29,17 +42,20 @@ function passwordchangeHandler(e)
         <h2 className='main-heading'> Welcome!</h2>
         <h3 className='heading'>Please,enter your details</h3>
         <div className='input-field linked'>
-          <input type='email' value={email} onChange={changeHandler} pattern='./@gmail.com' required placeholder='Enter Your Email'></input>
-          <div className='error-message'></div>
+          <input type='email' name='email' value={formData.email} onChange={changeHandler} pattern='./@gmail.com' required placeholder='Enter Your Email'></input>
+          <div className='error-message'><p>{errors.email}</p></div>
         </div>
-     {!isValidEmail && <p className='error-message'>{notice}</p>}
+     
         <div className='input-field'>
-          <input type='password'  onChange={passwordchangeHandler} required placeholder='Enter Your Password'></input>
-          <div className='linked'><Link to="/" className="link">Forget Password</Link>
-          </div>
+          <input type='password' name='password' value={formData.password} onChange={changeHandler} required placeholder='Enter Your Password'></input>
+          <div className='error-message'><p>{errors.password}</p></div>
+          <div className='linked'><Link to="/" className="link">Forget Password</Link></div>
+               
         </div>
         <div className='input-field  button'>
-          <button disabled={btndisable}>Sign in</button>
+          <button
+          //  disabled={btndisable}
+           >Sign in</button>
           <p>
             Don't have an account?{" "}<Link to='/' className="link">Sign up</Link>
           </p>
